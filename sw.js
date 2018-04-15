6c0764b30859be418004a75fd9df85d1e3f271d8
Daipayan Mukherjee <30791985+Satyr09@users.noreply.github.com>
2018-04-15 14:33:00 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,45 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+const cacheName = 'news-v1';
+
 const staticAssets = [
   './',
-  './index.php',
-  './style.css',
   './app.js',
+  './styles.css',
   './fallback.json',
+  './images/fetch-dog.jpg'
 ];
-workbox.precaching.precacheAndRoute(staticAssets
-);
 
-workbox.routing.registerRoute(
-  new RegExp('.*\.js'),
+self.addEventListener('install', async function () {
+  const cache = await caches.open(cacheName);
+  cache.addAll(staticAssets);
+});
 
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
-  workbox.strategies.networkFirst({
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  const url = new URL(request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(request));
+  } else {
+    event.respondWith(networkFirst(request));
+  }
+});
 
-  })
-);
-workbox.routing.registerRoute(
-  new RegExp ('https://newsapi.org/'),
+async function cacheFirst(request) {
+  const cachedResponse = await caches.match(request);
+  return cachedResponse || fetch(request);
+}
 
-  workbox.strategies.networkFirst({
-
-  })
-);
-
-
-workbox.routing.registerRoute(
-  /.*\.(png|jpg|jpeg|svg|gif)/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'news-images',
-    plugins : [ new workbox.expiration.Plugin({  maxEntries: 20,
-      }),
-    ]
-
-
-  })
-);
+async function networkFirst(request) {
+  const dynamicCache = await caches.open('news-dynamic');
+  try {
+    const networkResponse = await fetch(request);
+    dynamicCache.put(request, networkResponse.clone());
+    return networkResponse;
+  } catch (err) {
+    const cachedResponse = await dynamicCache.match(request);
+    return cachedResponse || await caches.match('./fallback.json');
+  }
+}
