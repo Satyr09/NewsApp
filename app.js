d124456252a63e2345555467365f5fc64e08924b
Daipayan Mukherjee <daisatyr09@gmail.com>
2018-04-15 10:35:56 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const main = document.querySelector('main');
+const sourceSelector = document.querySelector('#sourceSelector');
+const defaultSource = 'the-washington-post';
+
+window.addEventListener('load', e =>{
+  updateNews();
+  console.log('app file');
+  updateSources();
+  sourceSelector.value = defaultSource;
+
+  sourceSelector.addEventListener('change', e=>{
+    updateNews(e.target.value);
+  })
+
+  if('serviceWorker' in  navigator){
+    try {
+      navigator.serviceWorker.register('sw.js');
+      console.log('SW Registered');
+    } catch (e) {
+        console.log('SW Registration failed');
+    }
+
+  }
+});
+
+async function updateSources(){
+
+
+  const res = await fetch(`https://newsapi.org/v2/sources?apiKey=a955ea5818964bf6b533628ccad8572a
+`);
+  const json = await res.json();
+  sourceSelector.innerHTML = json.sources.map(src => `<option value = "${src.id}">${src.name}</option>`).join('\n');
+  console.log(json);
+}
+
+async function updateNews(source = defaultSource){
+    const res= await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=a955ea5818964bf6b533628ccad8572a`);
+    const json = await res.json();
+    main.innerHTML = json.articles.map(createArticle).join('\n');
+  
+};
+
+function createArticle(article){
+  return `
+    <div class="article">
+        <a href="${article.url}">
+            <h2>${article.title}</h2>
+            <img src="${article.urlToImage}">
+              <p>${article.description}</p>
+    </a>
+  </div>
+  `;
+
+
+}
---
+++ sw.js
@@ -0,0 +1,38 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+const staticAssets = [
+  './',
+  './style.css',
+  './app.js',
+  './fallback.json',
+];
+workbox.precaching.precacheAndRoute(staticAssets
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+
+
+  workbox.strategies.networkFirst({
+
+  })
+);
+workbox.routing.registerRoute(
+  new RegExp ('https://newsapi.org/'),
+
+  workbox.strategies.networkFirst({
+
+  })
+);
+
+
+workbox.routing.registerRoute(
+  /.*\.(png|jpg|jpeg|svg|gif)/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'news-images',
+    plugins : [ new workbox.expiration.Plugin({  maxEntries: 20,
+      }),
+    ]
+
+
+  })
+);
